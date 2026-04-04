df4bd6af3bb1be59f45938555d103111c0ec264d
Bob Jacobsen <jacobsen@mac.com>
2026-04-04 10:49:45 -0400

    Provide mechanism to delist windows from Window menu

--- java/src/jmri/util/WindowMenu.java
+++ java/src/jmri/util/WindowMenu.java
@@ -2,6 +2,7 @@ package jmri.util;
 
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.JCheckBoxMenuItem;
@@ -21,12 +22,18 @@ public class WindowMenu extends JMenu implements javax.swing.event.MenuListener 
     private Frame parentFrame; // Keep note of the window containing the menu
     private List<JmriJFrame> framesList; // Keep the list of windows, in order to find out which window was selected
 
+    static List<String> ignoredFrames = new ArrayList<String>();  // list of frames to not display
+    
     public WindowMenu(WindowInterface wi) {
         super(Bundle.getMessage("MenuWindow"));
         parentFrame = wi.getFrame();
         addMenuListener(this);
     }
 
+    static public void setIgnoredFrames(List<String> frames) {
+        ignoredFrames = frames;
+    }
+    
     @Override
     public void menuSelected(MenuEvent e) {
         String windowName;
@@ -58,9 +65,15 @@ public class WindowMenu extends JMenu implements javax.swing.event.MenuListener 
         for (int i = 0; i < framesNumber; i++) {
             JmriJFrame iFrame = framesList.get(i);
             windowName = iFrame.getTitle();
+            
             if (windowName.equals("")) {
                 windowName = "Untitled";
             }
+            
+            if (ignoredFrames.contains(windowName)) {
+                continue;
+            }
+            
             JCheckBoxMenuItem newItem = new JCheckBoxMenuItem(new AbstractAction(windowName) {
                 @Override
                 public void actionPerformed(ActionEvent e) {
